393d0b961321fc778c1b365b9a72916aa92dbfa5
Tiago Farias <tiagofarias.xtf@gmail.com>
2017-05-14 01:21:38 -0300

    Adicionando classe Player

--- src/java/quiz/models/Player.java
+++ src/java/quiz/models/Player.java
@@ -1,14 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package quiz.models;
+
+import java.util.UUID;
+import java.util.ArrayList;
 
 /**
  *
  * @author jeffersoncn
+ * @author Tiago Farias
  */
-public class Player {
+public final class Player {
     
+    private UUID id;
+    private String name;
+    public static ArrayList<Player> players;
+    
+    public Player(UUID id, String name){
+        this.setId(id);
+        this.setName(name);
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public static void setPlayer(Player player) throws ClassNotFoundException {
+        if(!(player instanceof Player)){
+            throw new ClassNotFoundException(player+" must be an instance of Player");
+        }
+        
+        if(!(players instanceof ArrayList)){
+            players = new ArrayList<>();
+        }
+        players.add(player);
+    }    
 }
