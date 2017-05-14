80df9fc46ad67d798cd9a3b13f8d6e87a6b85cb4
Victor Schleumer <victor.schleumer@gmail.com>
2017-05-14 12:00:30 -0300

    Adiciona classe Score

---
+++ src/java/quiz/models/Score.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz.models;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author schle
+ */
+public class Score {
+
+    private int score;
+    private Player player;
+    public static ArrayList<Score> scores = new ArrayList<Score>();
+
+    public ArrayList<Score> getRanking() {
+        return scores;
+    }
+
+    public void setRanking(Score score) {
+        scores.add(score);
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public Score(int score, Player player) {
+        this.score = score;
+        this.player = player;
+    }
+
+}
